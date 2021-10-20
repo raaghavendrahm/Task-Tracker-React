1660b71985f3868833fe74d8e76cf3c83de83250
raaghavendrahm <raaghavendrahm@gmail.com>
2021-10-20 13:37:14 +0530

    toggleReminder function is created in App component and passed as onToggle prop to Tasks to Task that toggle the boolean value of "reminder" of the task which is double clicked. Using conditionals, a new class "reminder" is added to that task if the boolean value is true for that task and is added with a green left border

--- src/App.js
+++ src/App.js
@@ -32,12 +32,26 @@ function App() {
     // Display the tasks whose id is not as same as the id of the task whose 'x' is clicked. So, doesn't display the task whose 'x' is clicked.
   };
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+    // Toggle the boolean value of reminder of the task which is double clicked. A green left border is added to it which is styled in Task component by adding 'reminder' class.
+  };
+
   return (
     <div className="App">
       <div className="container">
         <Header title="TT" />
         {tasks.length > 0 ? (
-          <Tasks tasks={tasks} onDelete={deleteTask} />
+          <Tasks
+            tasks={tasks}
+            onDelete={deleteTask}
+            onToggle={toggleReminder}
+          />
         ) : (
           'No Tasks To Show'
         )}
