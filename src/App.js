0144091f847446456eebfd9f805766a09fd5533a
raaghavendrahm <raaghavendrahm@gmail.com>
2021-10-20 12:54:38 +0530

    deleteTask function is created in App component that is passed as props from App to Tasks to Task

--- src/App.js
+++ src/App.js
@@ -26,11 +26,17 @@ function App() {
     },
   ]);
 
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+    // Display the tasks whose id is not as same as the id of the task whose 'x' is clicked. So, doesn't display the task whose 'x' is clicked.
+  };
+
   return (
     <div className="App">
       <div className="container">
         <Header title="TT" />
-        <Tasks tasks={tasks} />
+        <Tasks tasks={tasks} onDelete={deleteTask} />
       </div>
     </div>
   );
