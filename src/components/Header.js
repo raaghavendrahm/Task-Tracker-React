45e20c450cd2465104d3062d321c2c094500220d
raaghavendrahm <raaghavendrahm@gmail.com>
2021-10-21 13:53:19 +0530

    To show Add button only in home page, useLocation hook is used in Header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import Button from './Button';
+import { useLocation } from 'react-router';
 
 const Header = ({ title, onAddClick, showAddTaskValue }) => {
+  const location = useLocation();
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        color={showAddTaskValue ? 'red' : 'green'}
-        text={showAddTaskValue ? 'Close' : 'Add'}
-        onClick={onAddClick}
-      />
+      {location.pathname === '/' && (
+        <Button
+          color={showAddTaskValue ? 'red' : 'green'}
+          text={showAddTaskValue ? 'Close' : 'Add'}
+          onClick={onAddClick}
+        />
+      )}
     </header>
   );
 };
