232e772dca5a8cbb499735f37a5db9700ad66fc9
raaghavendrahm <raaghavendrahm@gmail.com>
2021-10-21 13:44:06 +0530

    When About link is clicked, it is displayed alongside AddTask and Tasks components. To prevent this, AddTask and Tasks are routed inside a single route. As multiple components are passed inside a Route, render is used with props as parameter

--- src/App.js
+++ src/App.js
@@ -79,16 +79,25 @@ function App() {
             onAddClick={() => setShowAddTask(!showAddTask)}
             showAddTaskValue={showAddTask}
           />
-          {showAddTask && <AddTask onAdd={addTask} />}
-          {tasks.length > 0 ? (
-            <Tasks
-              tasks={tasks}
-              onDelete={deleteTask}
-              onToggle={toggleReminder}
-            />
-          ) : (
-            'No Tasks To Show'
-          )}
+          <Route
+            path="/"
+            exact
+            render={(props) => (
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  'No Tasks To Show'
+                )}
+              </>
+            )}
+          />
+
           <Route path="/about" component={About} />
           <Footer />
         </div>
